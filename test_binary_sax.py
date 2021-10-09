9edbc961e03449c7cf8203c968ca6e40fd7980c3
George Papanotas <george.papanotas@spotawheel.com>
2021-10-09 15:12:19 +0300

    Created a function to return bit sequences up to a number. This will allow me to extend the SAX library to any alphabet size

--- test_binary_sax.py
+++ test_binary_sax.py
@@ -19,13 +19,35 @@
 
 # def calculateCardinality(wordLength):
 
+# I have created a function to extend SAX words to any arbitrary number
+# by making the characters bit strins
+
 import math
+import itertools
+
 
 
 def countBits(number):
     return int((math.log(number) / math.log(2)) + 1)
 
+# # Driver Code
+# num = 5
 
-# Driver Code
+# print(countBits(num))
+
+
+def bit_sequence(bits_needed, number):
+    bit_sequence = []
+    bin_num = str(bin(number - 1))[2:]
+    for x in map(''.join, itertools.product('01', repeat=bits_needed)):
+        print(f"{bin_num}, {x}")
+        bit_sequence.append(x)
+        if bin_num == x:
+            break
+
+    return bit_sequence
+
+
 num = 5
 print(countBits(num))
+print(bit_sequence(countBits(num), num))
