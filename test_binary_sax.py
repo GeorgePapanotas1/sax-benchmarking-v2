3a5afc02980f5c0f8988f8bf76d4acd9b23ea5a2
George Papanotas <george.papanotas@spotawheel.com>
2021-10-09 17:02:54 +0300

    I have extended the saxpy library to produce array of bit values in order to express alphabets grater than 20

--- test_binary_sax.py
+++ test_binary_sax.py
@@ -1,46 +1,23 @@
-# def ts_to_string(series, cuts):
-#     """A straightforward num-to-string conversion."""
-#     a_size = len(cuts)
-#     sax = list()
-#     for i in range(0, len(series)):
-#         num = series[i]
-#         # if teh number below 0, start from the bottom, or else from the top
-#         if(num >= 0):
-#             j = a_size - 1
-#             while ((j > 0) and (cuts[j] >= num)):
-#                 j = j - 1
-#             sax.append(idx2letter(j))
-#         else:
-#             j = 1
-#             while (j < a_size and cuts[j] <= num):
-#                 j = j + 1
-#             sax.append(idx2letter(j-1))
-#     return ''.join(sax)
-
-# def calculateCardinality(wordLength):
-
 # I have created a function to extend SAX words to any arbitrary number
 # by making the characters bit strins
 
 import math
 import itertools
-
+from saxpy.znorm import znorm
+from saxpy.paa import paa
+import pandas as pd
+from scipy.stats import norm
+import numpy as np
 
 
 def countBits(number):
     return int((math.log(number) / math.log(2)) + 1)
-
-# # Driver Code
-# num = 5
-
-# print(countBits(num))
 
 
 def bit_sequence(bits_needed, number):
     bit_sequence = []
     bin_num = str(bin(number - 1))[2:]
     for x in map(''.join, itertools.product('01', repeat=bits_needed)):
-        print(f"{bin_num}, {x}")
         bit_sequence.append(x)
         if bin_num == x:
             break
@@ -48,6 +25,46 @@ def bit_sequence(bits_needed, number):
     return bit_sequence
 
 
-num = 5
-print(countBits(num))
-print(bit_sequence(countBits(num), num))
+def ts_to_string(series, cuts):
+    """A straightforward num-to-string conversion."""
+    a_size = len(cuts)
+    sax = list()
+    #  Create the bit array.
+    sudo_bits = bit_sequence(countBits(a_size), a_size)
+    for i in range(0, len(series)):
+        num = series[i]
+        # if teh number below 0, start from the bottom, or else from the top
+        if(num >= 0):
+            j = a_size - 1
+            while ((j > 0) and (cuts[j] >= num)):
+                j = j - 1
+            sax.append(sudo_bits[j])
+        else:
+            j = 1
+            while (j < a_size and cuts[j] <= num):
+                j = j + 1
+            sax.append(sudo_bits[j-1])
+    return sax
+
+
+def convert_to_sax(dat, word_size, alphabet_size):
+    dat_znorm = znorm(dat)
+    dat_paa_3 = paa(dat_znorm, word_size)
+    sax_string = ts_to_string(dat_paa_3, cuts_for_asize(alphabet_size))
+    return sax_string
+
+
+def cuts_for_asize(num):
+    cuts = [-np.inf]
+    for i in range(1, num):
+        cuts.append(norm.ppf(i * 1/num))
+    return cuts
+
+
+word_size, alphabet_size = 100, 1024
+
+df = pd.read_csv ('first_year_subset.csv')
+dat1 = df['Temp'].to_numpy()
+sax1 = convert_to_sax(dat1, word_size, alphabet_size)
+print(sax1)
+
